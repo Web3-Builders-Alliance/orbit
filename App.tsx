4d2ca61e9fcc966608dbada18e1a0334cb1fd91e
Ahzam Akhtar <ahzam8186@gmail.com>
2023-08-14 00:34:26 +0530

    navigation done

--- App.tsx
+++ App.tsx
@@ -4,30 +4,44 @@ import React from 'react';
 import {SafeAreaView, StyleSheet} from 'react-native';
 import {AuthorizationProvider} from './components/providers/AuthorizationProvider';
 import {Header} from './components/Header';
-import { NavigationContainer } from '@react-navigation/native';
 import Tabs from './navigation/tabs';
 import MainScreen from './screens/MainScreen';
+import {NavigationContainer , DefaultTheme} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 const DEVNET_ENDPOINT = clusterApiUrl('devnet');
+
+const MyTheme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    background: 'black'
+  },
+};
 
 export default function App() {
   return (
-    <>
-   
     <ConnectionProvider
-      config={
-        {commitment: 'processed'}
-        }
+      config={{commitment: 'processed'}}
       endpoint={DEVNET_ENDPOINT}>
       <AuthorizationProvider>
         <SafeAreaView style={styles.shell}>
-          {/* <Header /> */}
-          <MainScreen />
-
+          <NavigationContainer theme={MyTheme}>
+            <Stack.Navigator
+            screenOptions={{
+              headerShown : false
+            }}
+            >
+              <Stack.Screen
+                name="Home"
+                component={MainScreen}
+              />
+            </Stack.Navigator>
+          </NavigationContainer>
         </SafeAreaView>
       </AuthorizationProvider>
     </ConnectionProvider>
-    </>
-    
   );
 }
 
